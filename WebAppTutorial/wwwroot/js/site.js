b2c19042f0656607b3d639dbce3e1e2d1629da87
JamTerp79 <terpening.james@gmail.com>
2023-07-15 20:17:11 -0500

    Add project files.

---
+++ WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// JavaScript animation code
+const element = document.getElementById("animated-box");
+
+element.onclick = function () {
+    element.style.animationPlayState = 'paused';
+}
+
+// Write your Javascript code.
